33ef970d5d9dd7cb63ec4e8d2dc3c3b443def856
Anton <tosha.grafkof@yandex.ru>
2022-01-13 19:24:42 +0300

    добавлены страницы лицензия и расписание

---
+++ web_racing_kp/Controllers/UserTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using web_racing_kp.Models;
+
+namespace web_racing_kp.Controllers
+{
+    public class UserTablesController : Controller
+    {
+        private racing_dbEntities1 db = new racing_dbEntities1();
+
+        // GET: UserTables
+        public ActionResult Index()
+        {
+            return View(db.UserTable.ToList());
+        }
+
+        // GET: UserTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTable.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // GET: UserTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserTables/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_user,Status,Name,Name_Team,Racing_level,Points_in_seasion,Open_close_wheets")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTable.Add(userTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userTable);
+        }
+
+        // GET: UserTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTable.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: UserTables/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_user,Status,Name,Name_Team,Racing_level,Points_in_seasion,Open_close_wheets")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userTable);
+        }
+
+        // GET: UserTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTable.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: UserTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserTable userTable = db.UserTable.Find(id);
+            db.UserTable.Remove(userTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ web_racing_kp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_racing_kp.MvcApplication" Language="C#" %>
